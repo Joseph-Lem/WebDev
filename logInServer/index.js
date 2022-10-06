8bc3b4922588f4185f38490cecb6c260731d03bd
Joseph Lemieux <josephlemieux@Josephs-MacBook-Air.local>
2022-10-06 18:47:28 -0400

    ahhh

--- logInServer/index.js
+++ logInServer/index.js
@@ -5,8 +5,8 @@ const port = 3000
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-
-let userInfo = [{firstname:" ", lastname: "", username: "", password:""}];
+//firstname:" ", lastname: "", username: "", password:""
+let userInfo = [{firstname:"joe", lastname:"lem", username: "lem", password:"joe"}];
 
 function htmlStart(res){
     res.write(`<!DOCTYPE html>
@@ -24,25 +24,69 @@ function htmlEnd(res){
 </html>`)
 res.end();
 }
+let userLoggedIn = false;
 
 app.post('/login', (req, res) => {
-    console.log(req.body.username);
-    console.log(req.body.password);
+    let enteredUsername = req.body.username;
+    let enteredPassword = req.body.password;
+    for(let x of userInfo){
+        if ((enteredUsername === x.username) && (enteredPassword === x.password)){
+            console.log('correct, youre logged in');
+            userLoggedIn = true;
+            res.redirect('LPBFW.html');
+            return;
+        }
+        else{
+        }
+    }
     htmlStart(res, "something cool");
+    res.write(
+        "incorrect login or password, try again"
+    );
     htmlEnd(res);
 });
 app.post('/register', (req, res) => {
-    console.log(req.body.firstname);
-    console.log(req.body.lastname);
-    console.log(req.body.username);
-    console.log(req.body.password);
-    userInfo.push({firstname: req.body.firstname, 
+    let alreadyTaken = false;
+    for(let z of userInfo){
+        let newUsername = req.body.username;
+        if ((newUsername === z.username)){
+            alreadyTaken = true;
+        }
+        else{
+        }
+    }
+    if(!alreadyTaken){
+        userInfo.push({firstname: req.body.firstname, 
             lastname: req.body.lastname, 
             username: req.body.username, 
             password: req.body.password});
+            res.redirect('login.html')
+            return;
+    }
+    else{
+        res.write("username taken try a new one.");
+    }
     console.log(userInfo);
     htmlStart(res, "something cool");
+    res.write('brody');
     htmlEnd(res);
+});
+app.post('/LPBFW', (req, res)=> {
+    userLoggedIn = false;
+    res.redirect('index.html');
+    return;
+    htmlStart(res, "yuh");
+    htmlEnd(res);
+});
+app.use('/LPBFW', (req,res,next)=> {
+//restrict users that are not loggedin 
+if(!userLoggedIn){
+    res.redirect('index.html');
+    console.log('you cant be here');
+    return;
+}
+else{
+}
 });
 
 app.listen(port, () => {
